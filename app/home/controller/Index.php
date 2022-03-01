dafc2d18bc3f080243b172f6389ddbab742ad8b4
小黑 <61656077+xiaoheiba@users.noreply.github.com>
2022-03-01 16:22:15 +0800

    更新项目

--- app/home/controller/Index.php
+++ app/home/controller/Index.php
@@ -3,10 +3,12 @@ declare (strict_types = 1);
 
 namespace app\home\controller;
 
+
+
 class Index
 {
     public function index()
     {
-        return '您好！这是一个[home]示例应用';
+        return view('/pages/index');
     }
 }
